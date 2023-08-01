7ee14a4ee76398858753e4b2bc5a5d354becd446
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-08-01 17:16:19 +0300

    Add get wallet balances command

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,8 @@ import { ethers } from "ethers";
 export const ConfigParams = {
     INFURA_PROJECT_URL:process.env.INFURA_PROJECT_URL || "",
     BOT_TOKEN:process.env.BOT_TOKEN || "",
+    WHITELISTED_USERS:[541365365],
+    TELEGRAM_DELETE_MESSAGE_INTERVAL:process.env.TELEGRAM_DELETE_MESSAGE_INTERVAL
 }
 
 export const PRIVATE_KEYS = process.env.PRIVATE_KEYS?.split(',').filter(Boolean) || [];
--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -1,3 +1,6 @@
+import { bot } from "../bot/bot"
+import { ConfigParams } from "../config"
+
 export const normalizeeMessage = (message: string) => {
     return message
         .replaceAll("_", "\\_")
@@ -12,4 +15,29 @@ export const normalizeeMessage = (message: string) => {
         .replaceAll("-", "\\-")
         .replaceAll("!", "\\!")
         
+}
+
+
+export const sendMessage = async (message: string, delete_message?: boolean) => {
+    try {
+        for (const id of ConfigParams.WHITELISTED_USERS) {
+            await bot?.telegram?.sendMessage(id, normalizeeMessage(message), {
+                parse_mode: "MarkdownV2",
+                disable_web_page_preview: true
+            }).then(({ message_id }) => {
+                if (delete_message) {
+                    setTimeout(
+                        () => {
+                            bot.telegram.deleteMessage(id, message_id),
+                                ConfigParams.TELEGRAM_DELETE_MESSAGE_INTERVAL!
+                        }
+                    )
+                }
+            }).catch((error: any) => {
+                console.log("error:", error)
+            })
+        }
+    } catch (error: any) {
+        console.log("error:", error)
+    }
 }
