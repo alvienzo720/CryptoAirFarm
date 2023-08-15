58643f68acfb011cca2d0ce6f5f9b1e77d30845e
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-08-15 19:38:25 +0300

    Add all buy commands

--- src/utils/ethPrice.ts
+++ src/utils/ethPrice.ts
@@ -6,8 +6,6 @@ export async function getEthPrice(){
     const url =  `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${symbol}`;
     const response = await axios.get(url, { headers: { 'X-CMC_PRO_API_KEY': ConfigParams.COINMARKET } });
     const price = parseInt(response.data.data[symbol].quote.USD.price).toFixed(2); // Accessing price through the quote object
-
-    console.log("Eth price is ,", price);
     return price; // Returning the price directly
 }
 
